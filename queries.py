771b8363abb8effad6ffa1c40f46b2e812238e26
reece palmer <reecelpalmer@gmail.com>
2023-10-20 15:48:29 +0100

    Progress on 8 tests passing

--- queries.py
+++ queries.py
@@ -7,19 +7,17 @@ def get_average_purchase(db):
         CustomerID,
         AVG(OrderAmount) AS AverageOrderedAmount
     FROM (
-    SELECT
+        SELECT
             o.CustomerID,
             o.OrderID,
             SUM(ROUND(od.UnitPrice * od.Quantity, 2)) AS OrderAmount
-    FROM
-            orders o
-    JOIN
-            orderdetails od ON od.OrderID = o.OrderID
-    GROUP BY o.CustomerID, o.OrderID
+        FROM orders o
+        JOIN orderdetails od ON od.OrderID = o.OrderID
+        GROUP BY o.CustomerID, o.OrderID
     ) AS Subquery
     GROUP BY CustomerID
     ORDER BY CustomerID;
-"""
+    """
 
     results = db.execute(query)
     results = results.fetchall()
@@ -28,11 +26,58 @@ def get_average_purchase(db):
 
 def get_general_avg_order(db):
     # return the average amount spent per order
-    pass  # YOUR CODE HERE
+    query = """
+    WITH TemporaryTableName AS (
+        SELECT
+            OrderID,
+            SUM(UnitPrice * Quantity) AS ProductAmount
+        FROM OrderDetails
+        GROUP BY OrderID
+    )
+    SELECT AVG(ProductAmount) AS GeneralAvgOrder
+    FROM TemporaryTableName;
+    """
+
+    result = db.execute(query)
+    general_avg_order = result.fetchone()[0]
+
+    return general_avg_order
 
 def best_customers(db):
     # return the customers who have an average purchase greater than the general average purchase
-    pass  # YOUR CODE HERE
+    query = """
+        WITH CustomerAvgOrders AS (
+            SELECT
+                c.customerid,
+                ROUND(SUM(od.UnitPrice * od.Quantity) / COUNT(DISTINCT o.OrderID), 2) AS AverageOrderedAmount
+            FROM
+                customers c
+            JOIN
+                orders o ON o.customerid = c.customerid
+            JOIN
+                orderdetails od ON od.orderid = o.orderid
+            GROUP BY
+                o.customerid
+        ),
+        GeneralAverage AS (
+            SELECT ROUND(SUM(od.UnitPrice * od.Quantity) / COUNT(DISTINCT o.OrderID), 2) AS GeneralAverageOrderedAmount
+            FROM
+                orders o
+            JOIN
+                orderdetails od ON od.orderid = o.orderid
+        )
+        SELECT
+            cao.customerid,
+            cao.AverageOrderedAmount
+        FROM CustomerAvgOrders cao, GeneralAverage gao
+        WHERE cao.AverageOrderedAmount > gao.GeneralAverageOrderedAmount
+        ORDER BY cao.AverageOrderedAmount DESC
+    """
+    results = db.execute(query)
+    results = results.fetchall()
+
+    return results
+
 
 def top_ordered_product_per_customer(db):
     # return the list of the top ordered product by each customer
